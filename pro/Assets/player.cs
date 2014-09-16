1f1ada751ca593edefc34ba4598ee4325f4a0f42
soimar <wonder_112@naver.com>
2014-09-16 22:19:44 +0900

    0916
    캐릭터 애니메이션 추가

---
+++ pro/Assets/player.cs
@@ -0,0 +1,90 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class player : MonoBehaviour {
+	public Animator ani;
+	public enum PLAYER_STATE {
+		NORMAL,
+		WALK,
+		JUMP,
+		SHOOT,
+		PUSH
+	};
+		
+	PLAYER_STATE playerState = PLAYER_STATE.NORMAL;		
+	
+	
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+		
+		
+		if (Input.GetKey(KeyCode.LeftArrow))
+		{
+			if (playerState != PLAYER_STATE.WALK)
+			{
+				ani.SetTrigger("walk");
+				playerState = PLAYER_STATE.WALK;
+			}
+			
+			transform.rotation = Quaternion.Euler( new Vector3(0,0,90));
+			transform.Translate(Vector3.up * 15 * Time.deltaTime);
+			
+		}
+		if(Input.GetKey(KeyCode.RightArrow))
+		{
+			if (playerState != PLAYER_STATE.WALK)
+			{
+				ani.SetTrigger("walk");
+				playerState = PLAYER_STATE.WALK;
+			}
+
+			transform.rotation = Quaternion.Euler( new Vector3(0,0,270));
+			transform.Translate(Vector3.up * 15 * Time.deltaTime);
+		}
+		
+		if(Input.GetKey(KeyCode.UpArrow))
+		{
+			if (playerState != PLAYER_STATE.WALK)
+			{
+				ani.SetTrigger("walk");
+				playerState = PLAYER_STATE.WALK;
+			}
+
+			transform.rotation = Quaternion.Euler( new Vector3(0,0,360));
+			transform.Translate(Vector3.up * 15 * Time.deltaTime);
+		}
+		if(Input.GetKey(KeyCode.DownArrow))
+		{
+			if (playerState != PLAYER_STATE.WALK)
+			{
+				ani.SetTrigger("walk");
+				playerState = PLAYER_STATE.WALK;
+			}
+
+			transform.rotation = Quaternion.Euler( new Vector3(0,0,180));
+			transform.Translate(Vector3.down * -15 * Time.deltaTime);
+		}
+		
+		
+		
+		if(Input.GetKeyUp(KeyCode.UpArrow) ||
+		   Input.GetKeyUp(KeyCode.DownArrow) ||
+		   Input.GetKeyUp(KeyCode.RightArrow) ||
+		   Input.GetKeyUp(KeyCode.LeftArrow))
+		{
+			ani.SetTrigger("normal");
+			playerState = PLAYER_STATE.NORMAL;
+		}
+		
+		
+	}
+	
+	
+	
+}
