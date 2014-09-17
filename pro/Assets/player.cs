e372b1b80d155c486605ee192cf100fe52fd209a
soimar <wonder_112@naver.com>
2014-09-17 22:17:48 +0900

    0917
    벽 충돌 해결
    첫번째 박스 옮기기 퍼즐!

--- pro/Assets/player.cs
+++ pro/Assets/player.cs
@@ -22,7 +22,7 @@ public class player : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
 	
-		
+		//캐릭터 이동
 		
 		if (Input.GetKey(KeyCode.LeftArrow))
 		{
@@ -71,19 +71,58 @@ public class player : MonoBehaviour {
 			transform.Translate(Vector3.down * -15 * Time.deltaTime);
 		}
 		
-		
-		
-		if(Input.GetKeyUp(KeyCode.UpArrow) ||
-		   Input.GetKeyUp(KeyCode.DownArrow) ||
-		   Input.GetKeyUp(KeyCode.RightArrow) ||
-		   Input.GetKeyUp(KeyCode.LeftArrow))
+		if(Input.GetKey(KeyCode.Z))
 		{
-			ani.SetTrigger("normal");
-			playerState = PLAYER_STATE.NORMAL;
+			if (playerState != PLAYER_STATE.PUSH)
+			{
+				ani.SetTrigger("push");
+				playerState = PLAYER_STATE.PUSH;
+			}
+
+			//transform.rotation = Quaternion.Euler( new Vector3(0,0,180));
 		}
 		
+		if (playerState == PLAYER_STATE.WALK)
+		{
+			if(Input.GetKeyUp(KeyCode.UpArrow) ||
+			   Input.GetKeyUp(KeyCode.DownArrow) ||
+			   Input.GetKeyUp(KeyCode.RightArrow) ||
+			   Input.GetKeyUp(KeyCode.LeftArrow))
+			{
+				ani.SetTrigger("normal");
+				playerState = PLAYER_STATE.NORMAL;
+			}
+		}
+		else if (playerState == PLAYER_STATE.PUSH)
+		{
+			if (Input.GetKeyUp(KeyCode.Z))
+			{
+				ani.SetTrigger("normal");
+				playerState = PLAYER_STATE.NORMAL;
+			}
+		}
 		
 	}
+	
+	
+	
+	void OnCollisionEnter2D(Collision2D collision)
+	{
+		print ("OK");
+		
+		if(playerState == PLAYER_STATE.PUSH)
+		{
+			if(collision.gameObject.tag == "box")
+			{
+				DistanceJoint2D joint = GetComponent<DistanceJoint2D>();
+			
+				joint.connectedBody = collision.rigidbody;
+				
+			}
+		}
+		
+	}
+	
 	
 	
 	
