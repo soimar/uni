4f545fc53e5c4b20931269fef1f42f088ba87891
soimar <wonder_112@naver.com>
2014-10-17 22:23:38 +0900

    10_17
    레이저 구현해야됨!

---
+++ pro/Assets/light.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class light : MonoBehaviour {
+	public GameObject beam;
+	public bool m_light = true;
+	GameObject clone_beam;
+	
+	// Use this for initialization
+	void Start () {
+		
+		clone_beam = (GameObject)Instantiate(beam, transform.position ,Quaternion.Euler(0,0,90));
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+		if(m_light == true)
+		{
+			
+			clone_beam.transform.localScale += new Vector3(0,0.2f,0);
+		}
+		
+	}
+	
+		void OnCollisionEnter2D(Collision2D collision)
+	{		
+		print ("ok");
+		m_light = false;
+
+	}
+}
