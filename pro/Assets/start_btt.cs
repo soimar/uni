ef1485ab5408ddca7b9a736d54cc18d7aaea0b35
soimar <wonder_112@naver.com>
2014-11-03 21:45:37 +0900

    11_03
    끝!!!!!!!!!!!!!!!

---
+++ pro/Assets/start_btt.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class start_btt : MonoBehaviour {
+	public GUITexture btt_UP;
+	public GameObject btt_UP_obj;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		GUI_Check ();
+	
+	}
+
+
+	void GUI_Check()
+	{
+		if (Input.GetMouseButtonDown (0)) 
+		{
+			if(btt_UP.HitTest(new Vector3(Input.mousePosition.x, Input.mousePosition.y, 0)))
+			{
+				Application.LoadLevel ("play");
+			}
+		}
+	}
+}
